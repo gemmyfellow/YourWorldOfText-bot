6d1ea7ac1dee1631d992ba5e24d3f47a5db6e173
gemmyfellow <171301123+gemmyfellow@users.noreply.github.com>
2024-06-09 23:32:44 +0200

    asyncio functionality

--- troll.py
+++ troll.py
@@ -1,15 +1,30 @@
 import websockets
-from websockets.sync.client import connect
+#from websockets.sync.client import connect
+from websockets.client import connect
 import json
 import time
 import argparse
+import asyncio
 import sys
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file" , help="The path to the textfile to insert." )
 parser.add_argument("-p","--pos", help="The absolute position of text on the map, given as y,x.")
 parser.add_argument("-m","--map" , help= "the id of the map to paste this on.")
 args = parser.parse_args(sys.argv[1:])
+
+
+def chunk(iterable, size):
+    if not iterable or size <= 0:
+        return []
+    i = 0
+    while i < len(iterable):
+        j = i + size
+        yield iterable[i:j]
+        i = j
+ 
+
 
 if not args.file:
     print( "no file found", file=sys.stderr)
@@ -38,25 +53,30 @@ def read_file_as_lines(file_path):
     
 
 
-def write_lines_at_pos(pos, lines, socket):
+async def write_lines_at_pos(pos, lines, socket):
     '''Use websocket to write all the given lines at a position in a map.'''
     for line_index in range(len(lines)):
         line = lines[line_index]
-        writes = []
         # make a websocket send request for each line
-        for x in range(len(line)):
-            # Tile pos is the tile where the character is inserted
+        chunks = chunk(line,30)
+        x = 0
+        for line_chunk in chunks:
             
-            tile_pos = [(pos[0]+line_index)//TILE_HEIGHT, (pos[1]+x)//TILE_WIDTH]
-            # Char pos is the position relative to the tile where a character is inserted
-            char_pos = [(pos[0]+line_index)%TILE_HEIGHT, (pos[1]+x)%TILE_WIDTH]
-            write = Write(line[x],tile_pos, char_pos)
-            writes.append(write)
-        # create request
-        write_req = WriteRequest(writes)
-        # send
-        message_write = json.dumps(write_req.to_dict())
-        socket.send(message_write)
+            writes = []
+            for i in range(len(line_chunk)):
+                # Tile pos is the tile where the character is inserted
+                tile_pos = [(pos[0]+line_index)//TILE_HEIGHT, (pos[1]+x+i)//TILE_WIDTH]
+                # Char pos is the position relative to the tile where a character is inserted
+                char_pos = [(pos[0]+line_index)%TILE_HEIGHT, (pos[1]+x+i)%TILE_WIDTH]
+                write = Write(line_chunk[i],tile_pos, char_pos)
+                writes.append(write)
+            x += len(line_chunk)
+            # create request
+            write_req = WriteRequest(writes)
+            # send
+            message_write = json.dumps(write_req.to_dict())
+            await socket.send(message_write)
+            await asyncio.sleep(0.15)
 
 # the request id in a request doesnt seem to be super important
 req_id = 1
@@ -136,13 +156,29 @@ url = f"wss://www.yourworldoftext.com/{map_id}/ws/"
 TILE_HEIGHT = 8
 TILE_WIDTH = 16
 
-        
-while True:
-    try:
-        with connect(url) as socket:
-            lines = read_file_as_lines(file_path)
-            write_lines_at_pos(pos, lines, socket)
-            print("Sent") 
-    except Exception as e:
-        # ensures any error encountered are handled and attempt can be tried again
-        print(f"Exception occurred: {e}")
+
+
+async def indef_write_lines_at_pos(pos, lines):
+    while True:
+        try:
+            async with connect(url) as socket:
+                await write_lines_at_pos(pos, lines, socket)
+                print("Sent") 
+        except Exception as e:
+            # ensures any error encountered are handled and attempt can be tried again
+            print(f"Exception occurred: {e}")
+
+async def main():
+    lines = read_file_as_lines(file_path)
+    workers = []
+    line_no = 0
+    for chunk_of_lines in chunk(lines,40):
+        workers.append(indef_write_lines_at_pos([pos[0]+line_no,pos[1]], chunk_of_lines))
+        line_no += len(chunk_of_lines)
+    await asyncio.gather(*workers)
+
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
